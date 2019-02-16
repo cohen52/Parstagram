8f5d849772fd12973ccd28416aeb9259ffdd936f
cohen52 <31905089+cohen52@users.noreply.github.com>
2019-02-16 11:43:56 -0500

    finished with gif

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -15,12 +15,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     
     var posts = [PFObject]()
+    var refreshControl: UIRefreshControl!
+    var numberOfPosts: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
 
         // Do any additional setup after loading the view.
     }
@@ -28,9 +34,23 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         
+        numberOfPosts = 20
+        
         let query = PFQuery(className:"Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = numberOfPosts
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func loadMorePosts(){
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts + 20
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
                 self.posts = posts!
@@ -45,6 +65,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if(indexPath.row > posts.count){
+            return cell
+        }
         let post = posts[indexPath.row]
         
         let user = post["author"] as! PFUser
@@ -59,6 +82,24 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.photoView.af_setImage(withURL: url)
         
         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count{
+            loadMorePosts()
+        }
+    }
+    
+    @objc func onRefresh(){
+        run(after: 2) {
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
     
 
